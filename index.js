446182afcade1d6f2f8917ae10d23a22c98114b1
Fruit <boskozarich@gmail.com>
2017-06-17 12:14:49 -0400

    Removed debug msg

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 module.exports = function afkrewards(dispatch) {
-
-	dispatch.hook('C_RECEIVE_PLAYTIME_EVENT_REWARD', 1, (event) => {
-	console.log('roll:'+event.roll+' column:'+event.column)
-	});
 	
 	dispatch.hook('S_PLAYTIME_EVENT_REWARD_DATA', 1, (event) => {
 		for(let i in event.items){
 			if(event.items[i].available){
-			console.log('sending roll: '+event.items[i].roll+' column: '+event.items[i].column+' av: '+event.items[i].available+' claimed: '+event.items[i].claimed);
 				dispatch.toServer('C_RECEIVE_PLAYTIME_EVENT_REWARD', 1, {
 					roll : event.items[i].roll,
 					column : event.items[i].column
