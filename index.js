4187658c206f69d721aefb1376515a24ed9404c3
Fruit <boskozarich@gmail.com>
2017-06-17 18:48:44 -0400

    update for new definitions

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ module.exports = function afkrewards(dispatch) {
 	
 	dispatch.hook('S_PLAYTIME_EVENT_REWARD_DATA', 1, (event) => {
 		for(let i in event.items){
-			if(event.items[i].available){
+			if(event.items[i].redeemable){
 				dispatch.toServer('C_RECEIVE_PLAYTIME_EVENT_REWARD', 1, {
-					roll : event.items[i].roll,
+					row : event.items[i].row,
 					column : event.items[i].column
 				});
 			}
